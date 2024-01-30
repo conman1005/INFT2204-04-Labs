0db2e05377bdc776a3a58f9d0c788958905c715a
conman1005 <connercullity@live.ca>
2024-01-30 18:38:35 -0500

    Finished Simple DOM Manipulation
    used createElement, insertBefore, and appendChild to get this part done.

---
+++ Lab 1/scripts/app.js
@@ -0,0 +1,35 @@
+// Get the NavBar
+const navItems = document.getElementById("navItems");
+
+// Change Products to Projects
+const productButton = document.getElementById("products");
+productButton.innerHTML = "Projects";
+
+// Add Human Resources to NavBar
+// create new list item
+const newNavItem = document.createElement("li");
+newNavItem.className = "nav-item";
+// create new nav-link
+const navLink = document.createElement("a");
+navLink.className = "nav-link";
+navLink.innerHTML = "Human Resources";
+navLink.id = "hrLink";
+// add new item to navbar
+newNavItem.appendChild(navLink);
+//navItems.appendChild(newNavItem);
+navItems.insertBefore(newNavItem, navItems.children[4]);
+
+// Create Bottom NavBar
+const navBottom = document.createElement("nav");
+navBottom.className = "navbar fixed-bottom navbar-light bg-light";
+// create nav container and append to navBottom
+const navDiv = document.createElement("div");
+navDiv.className = "container-fluid";
+navBottom.appendChild(navDiv);
+// create navbar brand and append to navDiv
+const navBrand = document.createElement("a");
+navBrand.className = "navbar-brand";
+navBrand.innerHTML = "&copy Copyright 2024";
+navDiv.appendChild(navBrand);
+// Add bottom bar to body
+document.body.appendChild(navBottom);
