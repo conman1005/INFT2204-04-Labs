e111a9805ab4ab8542c38a1a3b4d8bd85bd189c9
conman1005 <connercullity@live.ca>
2024-02-21 22:30:06 -0500

    comments
    form validation comments

--- Lab 1/scripts/register.js
+++ Lab 1/scripts/register.js
@@ -15,17 +15,23 @@ form.on("submit", function(e) {
 
     console.log("Username: " + vals[0] + " Password: " + vals[1] + values);
 
+    // Validate First Name
     if (vals[0].length < 2) { 
         $(errorMessage).text("Error: First Name must be at least 2 characters.");
-    } else if (vals[1].length < 2) { 
+    } // Validate Last Name
+    else if (vals[1].length < 2) { 
         $(errorMessage).text("Error: Last Name must be at least 2 characters.");
-    } else if (vals[2].length < 8) {
+    } // Validate Email Name
+    else if (vals[2].length < 8) {
         $(errorMessage).text("Error: Email must be at least 8 characters.");
-    } else if (vals[3] != vals[4]) {
+    } // Validate Matching Passwords
+    else if (vals[3] != vals[4]) {
         $(errorMessage).text("Error: Passwords entered must Match.");
-    } else if (vals[3].length < 6) {
+    } // Validate password length
+    else if (vals[3].length < 6) {
         $(errorMessage).text("Error: Password must be at least 6 characters.");
-    } else {
+    }
+    else {
         $(errorMessage).text("");
     }
 
